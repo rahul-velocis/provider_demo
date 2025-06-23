55da27ec10f6260788db36f7173d2b89c577f594
rahul-velocis <rahul@velociscore.com>
2025-06-23 18:20:47 +0545

    Color provider updated with color and colorName

--- lib/pages/color_page.dart
+++ lib/pages/color_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:provider_demo/constants/color_constants.dart';
+import 'package:provider_demo/provider/color_provider.dart';
 
 class ColorPage extends StatelessWidget {
   const ColorPage({super.key});
@@ -26,26 +28,46 @@ class ColorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Color Page')),
+      appBar: AppBar(
+        title: Row(
+          spacing: 10,
+          children: [
+            Text('Color Page'),
+            Text(
+              context.watch<ColorProvider>().colorName,
+              style: TextStyle(
+                color: context.watch<ColorProvider>().color,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: context.watch<ColorProvider>().color,
       body: ListView.builder(
         padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
         itemCount: ColorConstants.colorNames.length,
         itemBuilder: (context, index) {
-          final color = ColorConstants.colorNames[index];
+          final colorName = ColorConstants.colorNames[index];
+          final color = getColor(colorName);
           return InkWell(
-            onTap: () {},
+            onTap: () {
+              context.read<ColorProvider>().setColor(color);
+              context.read<ColorProvider>().setColorName(colorName);
+            },
             child: Container(
               decoration: BoxDecoration(
-                color: getColor(color),
+                color: color,
 
                 borderRadius: BorderRadius.circular(20),
+                border: BoxBorder.all(color: Colors.white, width: 5),
               ),
               padding: EdgeInsets.all(10),
               margin: EdgeInsets.only(bottom: 10),
               width: MediaQuery.sizeOf(context).width * 0.8,
               height: 100,
               child: Text(
-                color,
+                colorName,
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 25,
--- lib/provider/color_provider.dart
+++ lib/provider/color_provider.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 class ColorProvider with ChangeNotifier {
+  //colors
   Color _color = Colors.grey;
 
   Color get color => _color;
 
   void setColor(Color color) {
     _color = color;
+    notifyListeners();
+  }
+
+  //Color Name
+  String _colorName = 'Grey';
+  String get colorName => _colorName;
+
+  void setColorName(String newColorName) {
+    _colorName = newColorName;
   }
 }
